6679641f0d733fe180e4750a152fc279d6812c63
kyleyjackson <kjackson@bayviewglen.ca>
2024-10-05 19:18:01 -0400

    updated2

--- uni-work/assignments/assignment3/a3_300425781/a3_part1_300425781.py
+++ uni-work/assignments/assignment3/a3_300425781/a3_part1_300425781.py
@@ -3,14 +3,64 @@ def split_tester(N, d):
     # Your code for split_tester function goes here (instead of keyword pass)
     # Your code should include  dosctrings and the body of the function
     '''
-    (string, string) => string
+    (int, int) => Boolean
     
-    Takes a string N, consisting of numbers and another string d, which indicates the size of the groups that N will be split into
+    Takes a sequence of numbers N which are to be split into groups of length d. If N cannot be split into groups of length d, this functions returns False
+    Else, if the groups are increasing in numerical value, this function returns True, otherwise this function returns False
     '''
+
+    # length for iteration
+    length = len(str(N))
+    m = length
+
+    # new stuff
+    i = 0
+    sub = ''
+
+    # check if N can be split into groups of length d
+    if length % d != 0:
+        return False
+    
+    else:
+        while m <= length:
+            if len(sub) == d: # when split reaches desired length
+                if int(sub) > i: # check if increasing
+                    i = int(sub)
+                    sub = ''
+                else:
+                    return False
+            
+            else: # otherwise...
+                sub += str(N)[m]
+            
+            m += 1
+    
+    print(True)
+    return True
+            
+
 
 # you can add more function definitions here if you like       
 
+def welcomeMessage(str):
+    '''
+    (string) => None
 
+    Prints a welcome message board based with the specified string
+    '''
+
+    strLen = len(str) #length of inputted string
+    b = '*' # border
+    s = ' ' # empty spaces
+
+    #printing the board
+    print()
+    print(b * (strLen + 10))
+    print(b + s * (strLen + 8) + b)
+    print(b + '  __' + str + '__  ' + b)
+    print(b + s * (strLen + 8) + b)
+    print(b * (strLen + 10))
+    print()
             
 # main
 # Your code for the welcome message goes here, instead of name="Vida"
