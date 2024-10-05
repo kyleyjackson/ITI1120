4f57cb00fcbeb54b688ea9955b7abf641733a70a
kyleyjackson <kjackson@bayviewglen.ca>
2024-10-05 12:19:56 -0400

    a3 update

--- uni-work/assignments/assignment3/a3_300425781/a3_part1_300425781.py
+++ uni-work/assignments/assignment3/a3_300425781/a3_part1_300425781.py
@@ -1,2 +1,25 @@
 #! NO ARRAYS, TUPLES, OR SET DICTIONARIES
+def split_tester(N, d):
+    # Your code for split_tester function goes here (instead of keyword pass)
+    # Your code should include  dosctrings and the body of the function
+    pass
 
+# you can add more function definitions here if you like       
+
+
+            
+# main
+# Your code for the welcome message goes here, instead of name="Vida"
+name = "Vida"
+
+flag = True
+while flag:
+    question = input(name + ", would you like to test if a number admits an increasing-split of given size? ")
+    question = (question.strip()).lower()
+    if question == 'no':
+        flag = False
+    #YOUR CODE GOES HERE. The next line should be elif or else.
+    else:
+        pass
+        
+#finally your code goes here too.
